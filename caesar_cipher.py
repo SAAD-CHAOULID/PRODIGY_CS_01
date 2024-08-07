9d0ccd1cba0fdbe658223d3910af8d5436c58b83
SAAD-CHAOULID <saadchaoulid0@gmail.com>
2024-08-07 03:51:47 +0100

    caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,8 +4,7 @@ def encrypt(message,shift) :
     for letter in message : 
         if letter.isalpha() :    #Check if the character is a letter to encrypt it
             base = 65 if letter.isupper() else 97    #The first uppercase letter is 'A', which has an ASCII value of 65 and he first lowercase letter is 'a', which has an ASCII value of 97.
-            encrypted_letter = chr((ord(letter) - base + shift) % 26 + base)   
-    ''' Convert the letter to its ASCII value with ord(), subtract the base to get a zero-based index, add the shift value, use modulo 26 to wrap around the alphabet, add the base back to convertto the correct ASCII value, and finally use chr() to convert the ASCII value back to a character. '''
+            encrypted_letter = chr((ord(letter) - base + shift) % 26 + base)   #Convert the letter to its ASCII value with ord(), subtract the base to get a zero-based index, add the shift value, use modulo 26 to wrap around the alphabet, add the base back to convertto the correct ASCII value, and finally use chr() to convert the ASCII value back to a character.
         else :
             encrypted_letter = letter    #Non-alphabetic characters are added directly without encryption
         encrypted_message += encrypted_letter   
